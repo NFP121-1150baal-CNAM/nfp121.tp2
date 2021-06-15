4503fcf9024ae021ac96c487e4d6ba6073562dec
ghofranlaham1 <ghofram.laham@isae.edu.lb>
2021-06-15 22:49:25 +0300

    word

--- tp2/question2/FahrenheitCelsius.java
+++ tp2/question2/FahrenheitCelsius.java
@@ -15,12 +15,19 @@ public class FahrenheitCelsius{
       *  @param args ...
       */
      public static void main(String[] args){
-       try{
-       
-      
-       }catch(NumberFormatException nfe){
-           System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
-       }
+       int fahrenheit = 0;
+      float celsius = 0;
+      for(int i = 0; i<args.length; i++){
+      try{
+      fahrenheit = java.lang.Integer.parseInt(args[i]);
+      celsius = fahrenheitEnCelsius(fahrenheit);
+      System.out.println(fahrenheit + "\u00B0F -> " +((int)(celsius*10)
+      /10.0) + "\u00B0C");
+      }catch(NumberFormatException nfe){
+      System.out.println("error : " + nfe.getMessage());
+      }
+ 
+     }
        
      }
      
@@ -31,7 +38,7 @@ public class FahrenheitCelsius{
       */
      public static float fahrenheitEnCelsius( int f){
        // ...
-       return 0.F;	// à compléter	en remplaçant la valeur retournée par la fonction de conversion
+       return 0.F;    // à compléter    en remplaçant la valeur retournée par la fonction de conversion
        // ...
      }
 
